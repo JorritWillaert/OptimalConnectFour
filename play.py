af286e5545085ebcce558a70090a22b9cf657656
JorritWillaert <randpyprojects@gmail.com>
2020-12-11 12:12:06 +0100

    Add basic Player class

--- play.py
+++ play.py
@@ -1,10 +1,11 @@
 import game_functions as gf
+from player import Player
 
 def main():
     gf.print_rules()
     gamemode = gf.choose_gamemode()
-    #player1 = new Player(False)
-    #player2 = new Player(gamemode)
+    player1 = Player(False)
+    player2 = Player(gamemode)
     #play(player1, player2)
 
 if __name__ == "__main__":
